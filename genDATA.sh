def7fc57a15a577bf8c639c063ac4b86d235aa95
yjl450 <yjl450@nyu.edu>
2024-04-04 19:46:09 +0000

    add progress indication

--- genDATA.sh
+++ genDATA.sh
@@ -7,6 +7,7 @@ matrix_sizes=(32 64 128 256 511 512 513 1023 1024 1025 2047 2048)
 
 for size in "${matrix_sizes[@]}"
 do
+	printf 'Testing N=%d\t' $size
 	SUM=0
 	for i in {1..20}
 	do
@@ -15,6 +16,8 @@ do
 		SUM=$(awk '{print $1+$2}' <<<"${SUM} ${GFLOPS}")
 	done
 	AVG=$(awk '{print $1/20}' <<<"${SUM}")
+	printf 'AVG %s GFLOPS\n'  $AVG 
 	printf '%d\t%s\n' $size $AVG >> data.txt
 done
 
+printf 'Results saved in data.txt'
